ff6b3309b4ab2fdcbbaef9a60eeb7e192d127556
heidotrofimov <69143028+heidotrofimov@users.noreply.github.com>
2021-09-03 12:22:27 +0300

    Update download_target_products.py

--- download_target_products.py
+++ download_target_products.py
@@ -9,9 +9,5 @@ for filename in os.listdir("target_products"):
 f.close()
 
 os.system("~/miniconda3/envs/senpy/bin/python /home/heido/cvat-vsm/dias_old/main_engine.py -d s2_zip")
-
-for SAFE in os.listdir("s2_zip"):
-  if(".SAFE" in SAFE):
-    os.system("mv s2_zip/"+SAFE+" ../preprocessing/s2_zip/")
-    
-os.system("rm -r s2_zip/*")
+os.system("rm s2_zip/product*")
+os.system("mv s2_zip/* ../preprocessing/s2_zip/")
